2089e8e2de7107642f1cff5821af33e2936d87b4
HarinduJayatillek <52746592+HarinduJayatillek@users.noreply.github.com>
2020-03-17 15:24:10 +1100

    Add files via upload

---
+++ harindu-218696391.ino
@@ -0,0 +1,265 @@
+/*
+Harindu Jayatilleke
+Student ID: 218696391
+SIT210
+Photon Blinking
+*/
+
+int ledD7 = D7;
+
+void setup() {
+    pinMode(ledD7, OUTPUT);
+}
+
+void loop() {
+    
+    // first name begins
+    digitalWrite(ledD7, HIGH);
+    delay(200);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    digitalWrite(ledD7, HIGH);
+    delay(200);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    digitalWrite(ledD7, HIGH);
+    delay(200);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    digitalWrite(ledD7, HIGH);
+    delay(200);
+    digitalWrite(ledD7, LOW);
+    delay(200);            
+    // letter H
+    delay(1500);
+    
+    digitalWrite(ledD7, HIGH);
+    delay(200);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    digitalWrite(ledD7, HIGH);
+    delay(1000);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    // letter A
+    delay(1500);
+    
+    digitalWrite(ledD7, HIGH);
+    delay(200);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    digitalWrite(ledD7, HIGH);
+    delay(1000);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    digitalWrite(ledD7, HIGH);
+    delay(200);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    // letter R
+    delay(1500);
+    
+    digitalWrite(ledD7, HIGH);
+    delay(200);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    digitalWrite(ledD7, HIGH);
+    delay(200);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    // letter I
+    delay(1500);
+    
+    digitalWrite(ledD7, HIGH);
+    delay(1000);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    digitalWrite(ledD7, HIGH);
+    delay(200);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    // letter N
+    delay(1500);
+    
+    digitalWrite(ledD7, HIGH);
+    delay(1000);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    digitalWrite(ledD7, HIGH);
+    delay(200);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    digitalWrite(ledD7, HIGH);
+    delay(200);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    // letter D
+    delay(1500);
+    
+    digitalWrite(ledD7, HIGH);
+    delay(200);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    digitalWrite(ledD7, HIGH);
+    delay(200);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    digitalWrite(ledD7, HIGH);
+    delay(1000);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    // letter U
+    
+    delay(5000); 
+    // first name ends
+    
+    // last name begins 
+    digitalWrite(ledD7, HIGH);
+    delay(200);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    digitalWrite(ledD7, HIGH);
+    delay(1000);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    digitalWrite(ledD7, HIGH);
+    delay(1000);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    digitalWrite(ledD7, HIGH);
+    delay(1000);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    // letter J
+    delay(1500);
+    
+    digitalWrite(ledD7, HIGH);
+    delay(200);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    digitalWrite(ledD7, HIGH);
+    delay(1000);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    // letter A
+    delay(1500);
+    
+    digitalWrite(ledD7, HIGH);
+    delay(1000);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    digitalWrite(ledD7, HIGH);
+    delay(200);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    digitalWrite(ledD7, HIGH);
+    delay(1000);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    digitalWrite(ledD7, HIGH);
+    delay(1000);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    // letter Y
+    delay(1500);
+    
+    digitalWrite(ledD7, HIGH);
+    delay(200);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    digitalWrite(ledD7, HIGH);
+    delay(1000);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    // letter A
+    delay(1500);
+    
+    digitalWrite(ledD7, HIGH);
+    delay(1000);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    // letter T
+    delay(1500);
+    
+    digitalWrite(ledD7, HIGH);
+    delay(200);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    digitalWrite(ledD7, HIGH);
+    delay(200);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    // letter I
+    delay(1500);
+    
+    digitalWrite(ledD7, HIGH);
+    delay(200);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    digitalWrite(ledD7, HIGH);
+    delay(1000);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    digitalWrite(ledD7, HIGH);
+    delay(200);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    digitalWrite(ledD7, HIGH);
+    delay(200);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    // letter L
+    delay(1500);
+    
+    digitalWrite(ledD7, HIGH);
+    delay(200);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    digitalWrite(ledD7, HIGH);
+    delay(1000);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    digitalWrite(ledD7, HIGH);
+    delay(200);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    digitalWrite(ledD7, HIGH);
+    delay(200);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    // letter L
+    delay(1500);
+    
+    digitalWrite(ledD7, HIGH);
+    delay(200);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    // letter E
+    delay(1500);
+    
+    digitalWrite(ledD7, HIGH);
+    delay(1000);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    digitalWrite(ledD7, HIGH);
+    delay(200);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    digitalWrite(ledD7, HIGH);
+    delay(1000);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    // letter K
+    delay(1500);
+    
+    digitalWrite(ledD7, HIGH);
+    delay(200);
+    digitalWrite(ledD7, LOW);
+    delay(200);
+    // letter E
+    delay(1500);
+    
+    delay(5000); 
+    // last name ends
+    
+}
